26cf47e99dc2ac3438553bdcd5bf0473806b8c0a
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-08-26 13:35:16 +0200

    klokke

--- Oppstartsopplegg/java.js
+++ Oppstartsopplegg/java.js
@@ -2,11 +2,43 @@ window.onscroll = function() {scrollFunction()};
   function scrollFunction() {
     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
        document.getElementById("header").className = "stor";
-       /*document.getElementById("overskriftbilde").className = "stor";
-       document.getElementById("imgknapp").className = "stor";*/
     } else {
       document.getElementById("header").className = "";
-      /*document.getElementById("overskriftbilde").className = "";
-      document.getElementById("imgknapp").className = "";*/
     }
   }
+
+
+window.onload=function(){
+    const btn = document.querySelector(".lysmodus");
+    const theme = document.querySelector("#tema-link");
+     
+    btn.addEventListener("click", function() {
+      if (theme.getAttribute("href") == "stil.css") {
+        theme.href = "dark-stil.css";
+      } 
+
+      else {
+        theme.href = "stil.css";
+      }
+    });
+    }
+
+    
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("klokke").innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById("klokke").innerText = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
