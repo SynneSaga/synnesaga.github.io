9679fc0ada1b4904be7b55382b187f139ce00730
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-09-16 08:57:25 +0200

    ghh

--- Oppstartsopplegg/java.js
+++ Oppstartsopplegg/java.js
@@ -79,13 +79,13 @@ if (navigator.language == "nb.NO") {
 }
 else if (navigator.language == "en-US") {
   console.log("ingles");
-  document.getElementById("språk").innerHTML = ("Eww American");
+  document.getElementById("språk").innerHTML = ("Amerikanere...");
   //document.getElementById("#").className = "#";
 }
 
 else if (navigator.language == "en-GB") {
   console.log("ingles but better");
-  document.getElementById("språk").innerHTML = ("Bri'ish, ay?");
+  document.getElementById("språk").innerHTML = ("It's chewsday, innit?");
   //document.getElementById("#").className = "#";
 }
 
@@ -101,3 +101,114 @@ if (namn == "") {
 }
 
 else {document.getElementById("navnet").innerHTML = ("Velkommen, " + namn + "!")}
+
+//var info = { navn: namn, sprog: lang, time: tid};
+//var tid = h + ":" + m + ":" + s;
+
+//parseInt og parseFloat - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+console.log("Ting er: " + namn);
+console.log(`Ting er: ${namn}`);
+console.log(parseInt("1111",2)); //Binært system
+console.log(parseFloat("0.1"));
+
+console.log(parseInt("40 kr")); //Vil se vekk ifra kr siden ikke tall.
+console.log(Number("40 kr")); //Vil logge NaN, not a number, pga kr delen.
+
+//Boolean, True eller False - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+let amTired = true;
+if (amTired) { //kan være amTired == true, betyr samme greiene
+  namn = "Saga";
+  console.log("Jeg også");
+}
+else {
+  namn = namn; //egt unødvendig
+}
+
+//Tabeller og arrays - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+let arrayNamn = ["Saga", "Synne","SynneSaga"];
+console.log(arrayNamn);
+
+//Løkker - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+for (let hvertElement of arrayNamn) {
+  //document.getElementById("utskriftHvert").innerHTML = "<li>" + hvertElement + "</li>"; siden det er kun = vil den da skrive over første navnet og andre navnet, og du vil kun få SynneSaga.
+  document.getElementById("utskriftHvert").innerHTML += "<li>" + hvertElement + "</li>"; //Denne virker hvis du lager en div med id="utskrift" og tar document.getElementById("utskrift").innerHTML = arrayNamn;
+  //document.getElementById("utskriftHvert").innerHTML = document.getElementById("utskriftHvert").innerHTML + "<li>" + hvertElement + "</li>"; Denne virker også, men er knotete.
+  console.log(hvertElement);
+}
+
+//Disse to løkkene gjør akkuratt det samme^^vv
+for (let i = 0; i < arrayNamn.length; i++) {
+  console.log(arrayNamn[i]);
+}
+
+//Likhetstegn
+//pris = pris * 5; er samme som
+//pris *= 5;
+//Du tar operasjonen og setter før likhetstegnet, så kan du fjerne andre variabelen. Ting = Ting + 5; blir da Ting += 5;
+
+//lag ul med id="utskriftHvert"
+//<li>
+
+
+let question = "Hva er dette?";
+for (let bokstav of question) {
+  console.log(bokstav);
+
+  if(bokstav == "?") {
+    console.log("Spørsmål!");
+  }
+  else {
+    console.log("bokstav");
+  }
+}
+
+const tallPi = Math.PI; //mattebibliotek!
+console.log(tallPi)
+
+let desi = 6.9191
+console.log(Math.round(desi)); //runder av vanlig, får 7
+console.log(Math.floor(desi)); //runder ned, får 6
+console.log(Math.ceil(desi));  //runder opp, får 7
+
+//Lagre objekt
+let variabelNavn = {
+    navn: "Saga",
+    yrke: "elev",
+    etternavn: "Synne",
+    alder: 39
+};
+
+console.log(variabelNavn.yrke);
+
+
+let diffUtskrift = document.getElementById("diffUtskrift");
+let diffKnapp = document.getElementById("diffKnapp");
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length};
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+
+
+
+
+
+
+
